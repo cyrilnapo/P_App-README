eb3ad46ab8ea7f0fe556a8831e1af40f2994bba7
napo <napoleone.cyril@gmail.com>
2024-05-27 15:41:27 +0200

    starting new project because previous one was not launchable anymore

---
+++ app/app_readme/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+
+namespace app_readme
+{
+    public class Book
+    {
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Book> books = new ObservableCollection<Book>();
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+            books.Add(new Book { Image = "hpbook.jpg", Title = "{book.Title}", Author = "J.K. Rowling" });
+            books.Add(new Book { Image = "hpbook.jpg", Title = "Harry Potter et la Chambre des Secrets", Author = "J.K. Rowling" });
+            books.Add(new Book { Image = "hpbook.jpg", Title = "Harry Potter et le Prisonnier d'Azkaban", Author = "J.K. Rowling" });
+
+            Books = books;
+
+            BindingContext = this;
+        }
+
+        public ObservableCollection<Book> Books { get; set; }
+    }
+
+}
---
+++ app/app_readme/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace app_readme
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
